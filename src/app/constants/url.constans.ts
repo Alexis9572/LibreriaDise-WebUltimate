56ac812b6090b65ad443e461e3623013dd1a3383
Alexis9572 <torreshuamanialdairalexis@gmail.com>
2025-10-14 19:44:43 -0500

    LibreriaWeb

---
+++ src/app/constants/url.constans.ts
@@ -0,0 +1,9 @@
+export const dominio = "https://alexiswww-001-site1.ntempurl.com/";
+
+export const UrlConstans = {
+    rol: dominio + 'api/Rol',
+    producto: dominio + 'api/Producto',
+    auth: dominio + 'api/Auth',
+    pedido: dominio + 'api/Pedido',
+    devolucion: dominio + 'api/Devolucione'
+};
